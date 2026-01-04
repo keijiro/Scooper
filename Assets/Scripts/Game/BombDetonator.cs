0c25bbf33c64a292ae9eb6846989c74747d578b2
Keijiro Takahashi <keijiro@gmail.com>
2026-01-04 22:32:06 +0900

    Refactoring to BombDetonator

--- Assets/Scripts/Game/BombDetonator.cs
+++ Assets/Scripts/Game/BombDetonator.cs
@@ -16,35 +16,36 @@ public sealed class BombDetonator : MonoBehaviour
     PhysicsBody _body;
     float _elapsed;
 
+    const ulong DetectionMask = (1UL << (int)Categories.Scoop) |
+                                (1UL << (int)Categories.Item);
+
+    bool ShouldConsiderContact(PhysicsShape otherShape)
+      => (otherShape.contactFilter.categories.bitMask & DetectionMask) != 0;
+
     float GetMaxNormalSpeed()
     {
         var maxSpeed = 0f;
-        var bombSpeed = _body.linearVelocity.sqrMagnitude;
+        var fastBomb = _body.linearVelocity.sqrMagnitude > _speedThreshold;
 
         using var contacts = _body.GetContacts(Allocator.Temp);
 
-        for (var i = 0; i < contacts.Length; ++i)
+        foreach (var contact in contacts)
         {
-            var contact = contacts[i];
-            var otherShape = contact.shapeA.body == _body ? contact.shapeB : contact.shapeA;
-            var otherBody = otherShape.body;
-            if (!ShouldConsiderContact(otherShape, otherBody, bombSpeed)) continue;
+            var other = (contact.shapeA.body == _body) ? contact.shapeB : contact.shapeA;
+            if (!fastBomb && !ShouldConsiderContact(other)) continue;
 
             var manifold = contact.manifold;
+            if (manifold.pointCount == 0) continue;
+
             var points = manifold.points;
-            for (var p = 0; p < manifold.pointCount; ++p)
-                maxSpeed = Mathf.Max(maxSpeed, Mathf.Abs(points[p].normalVelocity));
+            maxSpeed = Mathf.Max(maxSpeed, Mathf.Abs(points.contactInfo0.normalVelocity));
+
+            if (manifold.pointCount == 1) continue;
+
+            maxSpeed = Mathf.Max(maxSpeed, Mathf.Abs(points.contactInfo1.normalVelocity));
         }
 
         return maxSpeed;
-    }
-
-    bool ShouldConsiderContact(PhysicsShape otherShape, PhysicsBody otherBody, float bombSpeed)
-    {
-        var otherCategories = otherShape.contactFilter.categories;
-        var scoopBit = (1UL << (int)Categories.Scoop) | (1UL << (int)Categories.Item);
-        if ((otherCategories.bitMask & scoopBit) != 0) return true;
-        return bombSpeed > _speedThreshold;
     }
 
     #endregion
